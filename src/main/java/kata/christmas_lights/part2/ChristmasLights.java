a14df3ea2fac75ff8d819eba3b80296fb2e188b8
Zouba Kamel <kamel.zouba@gmail.com>
2025-01-28 12:36:15 +0100

    partie 2: implémentation de la méthode toggle

--- src/main/java/kata/christmas_lights/part2/ChristmasLights.java
+++ src/main/java/kata/christmas_lights/part2/ChristmasLights.java
@@ -4,41 +4,37 @@ import lombok.Getter;
 
 @Getter
 public class ChristmasLights {
-    private final int[][] lights = new int[999][999];
+    private final int[][] lights = new int[1000][1000];
 
     public void turnOn(Coordinate begin, Coordinate end) {
-       turn(begin, end, true);
+        turn(begin, end, true, 1);
     }
 
     public void turnOff(Coordinate begin, Coordinate end) {
-        turn(begin, end, false);
+        turn(begin, end, false, 1);
     }
-
-    private void turn(Coordinate begin, Coordinate end, boolean on) {
-        for (int i = begin.x(); i <= end.x(); i++) {
-            for (int j = begin.y(); j <= end.y(); j++) {
-                if (lights[i][j] == 0 && !on)
-                    continue;
-                lights[i][j] += on?1:-1;
-            }
-        }
-    }
-
-/*    public void toggle(Coordinate begin, Coordinate end) {
-        for (int i = begin.x(); i <= end.x(); i++) {
-            for (int j = begin.y(); j <= end.y(); j++) {
-                lights[i][j] = !lights[i][j];
-            }
-        }
-    }*/
 
     public int getTotalBrightness() {
         int total = 0;
-        for (int i = 0; i < 999; i++) {
-            for (int j = 0; j < 999; j++) {
+        for (int i = 0; i < 1000; i++) {
+            for (int j = 0; j < 1000; j++) {
                 total += lights[i][j];
             }
         }
         return total;
+    }
+
+    public void toggle(Coordinate begin, Coordinate end) {
+        turn(begin, end, true, 2);
+    }
+
+    private void turn(Coordinate begin, Coordinate end, boolean on, int increment) {
+        for (int i = begin.x(); i <= end.x(); i++) {
+            for (int j = begin.y(); j <= end.y(); j++) {
+                if (lights[i][j] == 0 && !on)
+                    continue;
+                lights[i][j] += on ? increment : -increment;
+            }
+        }
     }
 }
